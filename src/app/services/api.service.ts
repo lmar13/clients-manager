3bb1fc8fa5fdb3facdd79e04b759a3dbf58a04f1
Łukasz Marciniak <lmarciniak@clickmeeting.com>
2025-07-22 01:09:24 +0200

    feat: add real api calls

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment.prod';
 import { Client } from '../models/client.model';
+import { Interest } from '../models/interest.model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,31 +13,34 @@ export class ApiService {
   private baseUrl = environment.apiUrl;
 
   getClients(): Observable<Client[]> {
-    console.log(this.baseUrl);
     return this.http.get<Client[]>(`${this.baseUrl}/getClients`);
   }
 
-  // addClient(data: Client) {
+  getClientById(id: string): Observable<Client> {
+    return this.http.get<Client>(`${this.baseUrl}/getClientById?id=${id}`);
+  }
 
-  // }
+  addClient(data: Client) {
+    this.http.post<Client>(`${this.baseUrl}/addClient`, data);
+  }
 
-  // updateClient(data: Client) {
+  updateClient(id: string, data: Client) {
+    this.http.put<Client>(`${this.baseUrl}/updateClient?id=${id}`, data);
+  }
 
-  // }
+  removeClient(id: string) {
+    this.http.delete<string>(`${this.baseUrl}/removeClient?id=${id}`);
+  }
 
-  // removeClient(id: string) {
+  getInterest(): Observable<Interest[]> {
+    return this.http.get<Interest[]>(`${this.baseUrl}/getInterests`);
+  }
 
-  // }
+  addInterest(data: Interest) {
+    this.http.post<Interest>(`${this.baseUrl}/addInterest`, data);
+  }
 
-  // getInterest(): Observable<Interest[]> {
-
-  // }
-
-  // addInterest(data: Interest) {
-
-  // }
-
-  // removeInterest(id: string) {
-
-  // }
+  removeInterest(id: string) {
+    this.http.delete<string>(`${this.baseUrl}/removeInterest?id=${id}`);
+  }
 }
