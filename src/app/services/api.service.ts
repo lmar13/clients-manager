681ad8cd2e3e515dcf3b75f39466737fce2a3c3e
Łukasz Marciniak <lmarciniak@clickmeeting.com>
2025-07-22 20:42:18 +0200

    feat: add api interceptor

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.prod';
 import { Client } from '../models/client.model';
 
 @Injectable({
@@ -9,25 +8,24 @@ import { Client } from '../models/client.model';
 })
 export class ApiService {
   private http = inject(HttpClient);
-  private baseUrl = environment.apiUrl;
 
   getClients(): Observable<Client[]> {
-    return this.http.get<Client[]>(`${this.baseUrl}/getClients`);
+    return this.http.get<Client[]>(`getClients`);
   }
 
   getClientById(id: string): Observable<Client> {
-    return this.http.get<Client>(`${this.baseUrl}/getClientById?id=${id}`);
+    return this.http.get<Client>(`getClientById?id=${id}`);
   }
 
   addClient(data: Client) {
-    this.http.post<Client>(`${this.baseUrl}/addClient`, data);
+    this.http.post<Client>(`addClient`, data);
   }
 
   updateClient(id: string, data: Client) {
-    this.http.put<Client>(`${this.baseUrl}/updateClient?id=${id}`, data);
+    this.http.put<Client>(`updateClient?id=${id}`, data);
   }
 
   removeClient(id: string) {
-    this.http.delete<string>(`${this.baseUrl}/removeClient?id=${id}`);
+    this.http.delete<string>(`removeClient?id=${id}`);
   }
 }
