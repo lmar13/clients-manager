6e32d5fc6436fd1320954909e56e00142eca94f5
Łukasz Marciniak <lmarciniak@clickmeeting.com>
2025-07-23 20:04:54 +0200

    feat: add lazy loading

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,52 +1,28 @@
 import { Routes } from '@angular/router';
-import { Step1Component } from '../components/step1/step1.component';
-import { Step2Component } from '../components/step2/step2.component';
-import { Step3Component } from '../components/step3/step3.component';
-import { FormsComponent } from '../pages/forms/forms.component';
-import { ListComponent } from '../pages/list/list.component';
-import { ClientResolver } from '../resolvers/client.resolver';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/list', pathMatch: 'full' },
+
   {
     path: 'list',
-    component: ListComponent,
+    loadComponent: () => import('../pages/list/list.component').then(m => m.ListComponent),
   },
+
   {
     path: 'add',
-    component: FormsComponent,
+    loadComponent: () => import('../pages/forms/forms.component').then(m => m.FormsComponent),
     children: [
       {
         path: 'step-1',
-        component: Step1Component,
+        loadComponent: () => import('../components/step1/step1.component').then(m => m.Step1Component),
       },
       {
         path: 'step-2',
-        component: Step2Component,
+        loadComponent: () => import('../components/step2/step2.component').then(m => m.Step2Component),
       },
       {
         path: 'step-3',
-        component: Step3Component,
-      },
-      { path: '', redirectTo: 'step-1', pathMatch: 'full' },
-    ],
-  },
-  {
-    path: 'edit/:id',
-    component: FormsComponent,
-    resolve: { client: ClientResolver },
-    children: [
-      {
-        path: 'step-1',
-        component: Step1Component,
-      },
-      {
-        path: 'step-2',
-        component: Step2Component,
-      },
-      {
-        path: 'step-3',
-        component: Step3Component,
+        loadComponent: () => import('../components/step3/step3.component').then(m => m.Step3Component),
       },
       { path: '', redirectTo: 'step-1', pathMatch: 'full' },
     ],
