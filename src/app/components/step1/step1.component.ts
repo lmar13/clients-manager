350fdd690eeff4dcb2737018474a1aeb5c74f45a
Łukasz Marciniak <lmarciniak@clickmeeting.com>
2025-07-23 21:40:10 +0200

    feat: add error message

--- src/app/components/step1/step1.component.ts
+++ src/app/components/step1/step1.component.ts
@@ -1,9 +1,11 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
+import { merge } from 'rxjs';
 import { phoneNumberValidator } from '../../shared/validators/phone-number.validator';
 import { Step1Facade } from '../../store/step1/step1.facade';
 import { CurrentStepFacade } from './../../store/currentStep/currentStep.facade';
@@ -21,6 +23,10 @@ export class Step1Component implements OnInit {
   private currentStepFacade$ = inject(CurrentStepFacade);
   form!: FormGroup;
 
+  nameErrorMessage = signal('');
+  surnameErrorMessage = signal('');
+  phoneErrorMessage = signal('');
+
   ngOnInit(): void {
     this.form = this.fb.group({
       name: ['', Validators.required],
@@ -31,6 +37,18 @@ export class Step1Component implements OnInit {
     this.step1Facade.step$.subscribe(data => {
       this.form.patchValue(data);
     });
+
+    merge(this.form.get('name')!.statusChanges, this.form.get('name')!.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateNameMessage());
+
+    merge(this.form.get('surname')!.statusChanges, this.form.get('surname')!.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateSurnameMessage());
+
+    merge(this.form.get('phone')!.statusChanges, this.form.get('phone')!.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updatePhoneMessage());
   }
 
   next() {
@@ -43,5 +61,34 @@ export class Step1Component implements OnInit {
       });
       this.router.navigate(['add/step-2']);
     }
+  }
+
+  updateNameMessage() {
+    const control = this.form.get('name');
+    if (control?.hasError('required')) {
+      this.nameErrorMessage.set('Name is required');
+    } else {
+      this.nameErrorMessage.set('');
+    }
+  }
+
+  updateSurnameMessage() {
+    const control = this.form.get('surname');
+    if (control?.hasError('required')) {
+      this.surnameErrorMessage.set('Surname is required');
+    } else {
+      this.surnameErrorMessage.set('');
+    }
+  }
+
+  updatePhoneMessage() {
+    const control = this.form.get('phone');
+    if (control?.hasError('required')) {
+      this.phoneErrorMessage.set('Phone is required');
+    }
+    if (control?.hasError('invalidPhone')) {
+      this.phoneErrorMessage.set('Invalid phone number');
+    }
+    return '';
   }
 }
