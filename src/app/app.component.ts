d647aaae69f1cdc9dd12fde116bf68701888986b
Łukasz Marciniak <lmarciniak@clickmeeting.com>
2025-07-23 22:40:41 +0200

    feat: when reload go to main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, HostListener } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { OrientationCheckComponent } from './components/orientation-check/orientation-check.component';
 
 @Component({
@@ -8,12 +8,18 @@ import { OrientationCheckComponent } from './components/orientation-check/orient
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private router = inject(Router);
+
   title = 'clients-manager';
   isPortrait = window.innerHeight > window.innerWidth;
 
   @HostListener('window:resize')
   onResize() {
     this.isPortrait = window.innerHeight > window.innerWidth;
+  }
+
+  ngOnInit() {
+    this.router.navigateByUrl('/'); // <-- wymusza przekierowanie na główną
   }
 }
