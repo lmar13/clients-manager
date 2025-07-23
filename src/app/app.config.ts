21b05efdc288073801591667aab5fca60080bbbf
Łukasz Marciniak <lmarciniak@clickmeeting.com>
2025-07-23 20:00:03 +0200

    feat: add lazy loading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angul
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { apiUrlInterceptor } from './core/api-url.interceptor';
 import { routes } from './routes/app.routes';
+import { ClientsEffects } from './store/clients/clients.effects';
+import { clientsReducer } from './store/clients/clients.reducer';
 import { currentStepReducer } from './store/currentStep/currentStep.reducer';
 import { step1Reducer } from './store/step1/step1.reducer';
 import { step2Reducer } from './store/step2/step2.reducer';
@@ -15,7 +18,13 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([apiUrlInterceptor])),
-    provideStore({ step1: step1Reducer, step2: step2Reducer, currentStep: currentStepReducer }),
+    provideStore({
+      step1: step1Reducer,
+      step2: step2Reducer,
+      currentStep: currentStepReducer,
+      clients: clientsReducer,
+    }),
+    provideEffects([ClientsEffects]),
     ...(isDevMode() ? [provideStoreDevtools()] : []),
   ],
 };
